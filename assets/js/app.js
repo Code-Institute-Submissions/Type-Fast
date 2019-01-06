8fdbaf0abe974522457507396b139e93b90c8ce5
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-01-06 22:44:55 +0000

    JSON file added with words and difficulty settings / app.js can read words.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,32 @@ const reset = $('#reset-game');
 const start = $('#start-game');
 const instructions = $('#game-instructions');
 const difficulty = $('.difficulty');
+const difficulty_changer = $('#difficulty-changer');
+var game_difficultly = 0;
+
+var displayWords = [];
 
 
 $(document).ready(function(){
-    
-
+    obtainWords();
     // On load open instructions modal
     //instructions.modal('show');
     
     //Change Difficultly
-    
+    difficulty.on('click', function(){
+        if (difficulty_changer.html() == 'Easy') {
+            difficulty_changer.html('Moderate');
+            game_difficultly = 1;
+        } else if (difficulty_changer.html() == 'Moderate') {
+            difficulty_changer.html('Hard');
+            game_difficultly = 2;
+        } else if (difficulty_changer.html() == 'Hard') {
+            difficulty_changer.html('Easy');
+            game_difficultly = 0;
+        }
+
+        obtainWords();
+    });
 
     //Start the Game
     start.on('click', function(){
@@ -20,7 +36,8 @@ $(document).ready(function(){
         reset.css('display','initial');
 
 
-    })
+
+    });
 
 
     //Reset the Game 
@@ -29,8 +46,24 @@ $(document).ready(function(){
         reset.css('display','none');
 
 
-    })
+    });
     
     
 })
+
+//Game
+
+function obtainWords(){
+    $.getJSON("assets/js/json/words.json", function(data){
+        
+        $.each(data, function(diff, val){
+            displayWords.push(val);
+        });
+        
+        console.log(displayWords[game_difficultly]["words"][0]);
+
+    });
+}
+
+//Timer
 
