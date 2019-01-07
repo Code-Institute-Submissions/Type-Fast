66c13d72de0fd543c3407d2b8210317e0e0b5e10
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-01-07 23:57:19 +0000

    Added functions to check comp word & user word, increase score, display comp word

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,31 +2,37 @@ const reset = $('#reset-game');
 const start = $('#start-game');
 const instructions = $('#game-instructions');
 const difficulty = $('.difficulty');
-const difficulty_changer = $('#difficulty-changer');
+const difficultyChanger = $('#difficulty-changer');
+const displayChoice = $('#word-display');
+const userInput = $('#user-input');
+const userScore = $('#score');
+const timer = $('#timer-sec');
+
+
 var game_difficultly = 0;
+var score = 0;
 
 var displayWords = [];
 
 
 $(document).ready(function(){
     obtainWords();
+    gameTimer();
     // On load open instructions modal
     //instructions.modal('show');
     
     //Change Difficultly
     difficulty.on('click', function(){
-        if (difficulty_changer.html() == 'Easy') {
-            difficulty_changer.html('Moderate');
+        if (difficultyChanger.html() == 'Easy') {
+            difficultyChanger.html('Moderate');
             game_difficultly = 1;
-        } else if (difficulty_changer.html() == 'Moderate') {
-            difficulty_changer.html('Hard');
+        } else if (difficultyChanger.html() == 'Moderate') {
+            difficultyChanger.html('Hard');
             game_difficultly = 2;
-        } else if (difficulty_changer.html() == 'Hard') {
-            difficulty_changer.html('Easy');
+        } else if (difficultyChanger.html() == 'Hard') {
+            difficultyChanger.html('Easy');
             game_difficultly = 0;
         }
-
-        obtainWords();
     });
 
     //Start the Game
@@ -35,7 +41,7 @@ $(document).ready(function(){
         start.css('display', 'none');
         reset.css('display','initial');
 
-
+        game();
 
     });
 
@@ -44,26 +50,76 @@ $(document).ready(function(){
     reset.on('click', function(){
         start.css('display', 'initial');
         reset.css('display','none');
-
-
+        displayChoice.html('When start is clicked, the word will appear here.');
+        score = 0;
+        userScore.html(score);
+        userInput.val("");
     });
     
     
-})
+});
 
 //Game
 
 function obtainWords(){
+
+
     $.getJSON("assets/js/json/words.json", function(data){
         
         $.each(data, function(diff, val){
             displayWords.push(val);
+            
         });
-        
-        console.log(displayWords[game_difficultly]["words"][0]);
 
+        console.log(displayWords);
     });
 }
 
+function showWord() {
+    //Random Number
+    i = Math.floor(Math.random() * 2);
+
+    //Displays random word from array
+    displayChoice.html(displayWords[game_difficultly]["words"][i]);
+
+}
+
+function game() {
+    showWord();
+    userInput.on('input', function () {
+        if (wordsMatch()) {
+            userScore.html(score);
+            userInput.val("");
+            showWord();
+        } else {
+            reset.css('background-color', 'red')
+        }
+    });
+    
+
+}
+
+function wordsMatch(){
+    //Sees if that input is a match
+    if (userInput.val() === displayChoice.html()) {
+        //green fade out
+        score++;
+        return true
+    } else {
+        // wiggle red fade out
+        return false;
+    };
+}
 //Timer
+
+function gameTimer() {
+    t = 5;
+
+    setInterval(function(){
+        t--;
+        timer.html(t);
+    }, 1000)
+}
+
+//Status 
 
