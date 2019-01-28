e0cd07cd8f6f43b808f88ab51e9fa7bd3a831726
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-01-28 19:47:25 +0000

    Removed obtainAPIKey as it won't work with GitHub. Removed JSON with API key.

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,33 +51,26 @@ $(document).ready(function(){
     });
 });
 
-//Get API Key from JSON file
-function obtainAPIKey() {
-    $.ajax({
-        url: 'assets/js/json/wordnik-api-key.json',
-        async: false,
-        success: function(data) {
-            api = data.apiKey;
-        }
-    })
-
-    return api;
-}
-
 //Get Words from API
 function obtainWords() {
 
-    //Get Key
-    apiKey = obtainAPIKey();
+    apiLink = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=8a8ea569a8c2098c500040f66e2044252dfdbb24b1b12e11c";
 
-    apiLink = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=10&api_key=" + apiKey;
-
+    //Filters out any word with accents and puts them into an array
     $.getJSON(apiLink, function(data){ 
-	    displayWords.push(data);
-    })
+        
+        var regExp = new RegExp("^[a-z]*$");
 
-    //Filter through to remove words with accented letters.
+        for (i = 0, len = 10; i < len; i++){
+            
+            var word = data[i].word;
 
+            if (regExp.test(word)){
+                displayWords.push(data[i].word);
+            }
+            
+        }
+    });
 }
 
 //Show word from Wordnik API 
@@ -87,7 +80,7 @@ function showWord() {
     i = Math.floor(Math.random()*10);
 
     //word to display
-    arrayChoice = displayWords[0][i].word;
+    arrayChoice = displayWords[i];
 
     //display word
     displayChoice.html(arrayChoice);
@@ -254,7 +247,7 @@ function saveUser(user, overwrite) {
     } 
 
     var newHighscore = [
-    {name: user, LScore: score}
+        {name: user, LScore: score}
     ];
 
     localStorage.setItem(getDiffBoard, JSON.stringify(newHighscore));  
