7d6048df344cd2fc533b210c3bd1b9b5a851f74d
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-02-11 20:05:36 +0000

    Commit changes ready for GitHub deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node assets/js/bundle.js
--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -580,24 +580,10 @@ $(document).ready(function(){
     });
 });
 
-function obtainAPIKey() {
-    $.ajax({
-        url: 'assets/js/json/wordnik-api-key.json',
-        async: false,
-        success: function(data) {
-            api = data.apiKey;
-        }
-    })
-
-    return api;
-}
-
 //Get Words from API
 function obtainWords() {
 
-    apiKey = obtainAPIKey();
-
-    apiLink = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=100&api_key=" + apiKey;
+    apiLink = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=100&api_key=8a8ea569a8c2098c500040f66e2044252dfdbb24b1b12e11c";
 
     //Filters out any word with accents and puts them into an array
     $.getJSON(apiLink, function(data){ 
