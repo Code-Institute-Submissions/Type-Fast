8c4515f76ce4966d9471a994563cb7d33c4a92ff
Matt Edwards <38842422+msped@users.noreply.github.com>
2019-03-01 00:59:06 +0000

    Changed Jasmine Testing Spec and added more descripition to the testing

--- assets/jasmine-testing/spec/type-fast-spec.js
+++ assets/jasmine-testing/spec/type-fast-spec.js
@@ -1,37 +1,27 @@
-describe("Type Fast", function(){
-    describe("Words Match? hero & villian", function(){
-        it("Should return False", function(){
-            expect(wordsMatch('hero', 'villian')).toBe(false);
-        });
+describe("Type Fast", function () {
+
+    it("returns false for two unequal strings (hero & villian)", function () {
+        expect(wordsMatch('hero', 'villian')).toBe(false);
     });
 
-    describe("Words Match? tempest & tempest", function(){
-        it("Should return True", function(){
-            expect(wordsMatch('tempest', 'tempest')).toBe(true);
-        });
+    it("returns true for two equal strings (tempest & tempest)", function () {
+        expect(wordsMatch('tempest', 'tempest')).toBe(true);
     });
 
-    describe("Testing capital letters. Mercedes & mercedes", function(){
-        it("Should return False", function(){
-            expect(wordsMatch('Mercedes', 'mercedes')).toBe(false);
-        });
+    it("returns false for two unequal strings, one with a capitialised letter (Mercedes & mercedes)", function () {
+        expect(wordsMatch('Mercedes', 'mercedes')).toBe(false);
     });
 
-    describe("Testing capital letters. Ford & Ford", function(){
-        it("Should return True", function(){
-            expect(wordsMatch('Ford', 'Ford')).toBe(true);
-        });
+    it("returns true with two equal strings, both capitialised (Ford & Ford)", function () {
+        expect(wordsMatch('Ford', 'Ford')).toBe(true);
     });
 
-    describe("Testing hyphenated words. part-time & part time", function(){
-        it("Should return False", function(){
-            expect(wordsMatch('part-time', 'part time')).toBe(false);
-        });
+    it("returns false for two unequal strings, one hypgenated and one without a hyphen (part-time & part time)", function () {
+        expect(wordsMatch('part-time', 'part time')).toBe(false);
     });
 
-    describe("Testing hyphenated words. part-time & part-time", function(){
-        it("Should return True", function(){
-            expect(wordsMatch('part-time', 'part-time')).toBe(true);
-        });
+    it("returns true for two equal strings, both hyphenated (part-time & part-time)", function () {
+        expect(wordsMatch('part-time', 'part-time')).toBe(true);
     });
+
 });
